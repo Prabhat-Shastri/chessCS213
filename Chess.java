662fee0ff64411b51e7ae56d6115e7b064474431
Aiman <m.aimankoli@gmail.com>
2025-09-24 20:09:11 -0400

    init

--- Chess.java
+++ Chess.java
@@ -15,9 +15,40 @@ public class Chess {
 	 *         See the section "The Chess class" in the assignment description for details of
 	 *         the contents of the returned ReturnPlay instance.
 	 */
+    public static String getPlayer(){
+        return Player;
+    }
+    public static String[] getMoves(String move){
+        m = move.split(" ");
+        if (m.length>0){
+            return m;
+        }else{
+            // Raise error?
+            String [] e = String [0];
+            return e;
+        }
+
+    }
 	public static ReturnPlay play(String move) {
 
 		/* FILL IN THIS METHOD */
+        String [] temp = getMoves(move);
+        if (temp.length==1){
+            //resign?
+            return null;
+        }
+        if (temp.length==2){
+            //Classic move
+            //Check valid move
+            //check if puts us under check
+            //other normal play instructions
+            return null;
+        }
+        if (temp.length==3){
+            //asking for a draw
+            return null;
+        }
+
 		
 		/* FOLLOWING LINE IS A PLACEHOLDER TO MAKE COMPILER HAPPY */
 		/* WHEN YOU FILL IN THIS METHOD, YOU NEED TO RETURN A ReturnPlay OBJECT */
