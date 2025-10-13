5e77136c685e31d9a8ce4966fb8537651cad4607
Prabhat-Shastri <prabhat.shastri.19@gmail.com>
2025-10-13 04:09:46 -0400

    Rook.java

--- chess/Rook.java
+++ chess/Rook.java
@@ -1,48 +1,49 @@
 package chess;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Rook extends Piece {
-    
     public Rook(boolean isWhite, Position position) {
         super(isWhite, position);
     }
-    
     @Override
     public List<Position> getPossibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
         int currentRank = position.getRank();
         int currentFile = position.getFile();
-        
-        // Horizontal and vertical directions
         int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
+        //it only moves horizontally or vertically so these are the 4 options for its movement 
         for (int[] dir : directions) {
             for (int i = 1; i < 8; i++) {
-                Position newPos = new Position(currentFile + i * dir[0], currentRank + i * dir[1]);
-                if (!newPos.isValid()) break;
-                
+                int nextFile = currentFile + i * dir[0];
+                int nextRank = currentRank + i * dir[1];
+                Position newPos = new Position(nextFile,nextRank);
+                if (!newPos.isValid()) {
+                    break;
+                }
                 Piece target = board.getPiece(newPos);
                 if (target == null) {
-                    moves.add(newPos);
+                    moves.add(newPos); //if empty, valid
                 } else {
                     if (target.isWhite() != isWhite) {
                         moves.add(newPos);
+                        //if enemy piece then we can kill and all to moves
                     }
                     break;
                 }
             }
         }
-        
         return moves;
     }
-    
     @Override
     public ReturnPiece.PieceType getPieceType() {
-        return isWhite ? ReturnPiece.PieceType.WR : ReturnPiece.PieceType.BR;
+        if(isWhite)
+        {
+            return ReturnPiece.PieceType.WR;
+        }
+        else{
+            return ReturnPiece.PieceType.BR;
+        }
     }
-    
     @Override
     public char getSymbol() {
         return 'R';
