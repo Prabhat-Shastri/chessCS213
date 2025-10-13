629712943a593d36c0dcea7d114b6632e1dcbdfb
Prabhat-Shastri <prabhat.shastri.19@gmail.com>
2025-10-13 04:37:27 -0400

    knight.java

--- chess/Knight.java
+++ chess/Knight.java
@@ -1,44 +1,45 @@
 package chess;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Knight extends Piece {
-    
     public Knight(boolean isWhite, Position position) {
         super(isWhite, position);
     }
-    
     @Override
     public List<Position> getPossibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
         int currentRank = position.getRank();
         int currentFile = position.getFile();
-        
-        // Knight moves in L-shape
         int[][] knightMoves = {
             {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
             {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
         };
-        
+        //The horse moves in an L shape manner thats why its +-2 +-1 or +-1 +-2
         for (int[] move : knightMoves) {
-            Position newPos = new Position(currentFile + move[0], currentRank + move[1]);
+            int nextFile = currentFile + move[0];
+            int nextRank = currentRank + move[1];
+            Position newPos = new Position(nextFile,nextRank);
             if (newPos.isValid()) {
                 Piece target = board.getPiece(newPos);
                 if (target == null || target.isWhite() != isWhite) {
+                    //again, here it is the same as other pieces, either empty or has enemy piece 
                     moves.add(newPos);
+                    //only then we are able to add the move to our list of moves
                 }
             }
         }
-        
         return moves;
     }
-    
     @Override
     public ReturnPiece.PieceType getPieceType() {
-        return isWhite ? ReturnPiece.PieceType.WN : ReturnPiece.PieceType.BN;
+        if(isWhite)
+        {
+            return ReturnPiece.PieceType.WN;
+        }
+        else{
+            return ReturnPiece.PieceType.BN;
+        }
     }
-    
     @Override
     public char getSymbol() {
         return 'N';
