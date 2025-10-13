df4323c7a1101c6942ad7383533863795f0ae2b8
Prabhat-Shastri <prabhat.shastri.19@gmail.com>
2025-10-13 04:46:13 -0400

    piece.java

--- chess/Piece.java
+++ chess/Piece.java
@@ -1,32 +1,33 @@
 package chess;
-
 import java.util.List;
-
 public abstract class Piece {
     protected boolean isWhite;
     protected Position position;
     protected boolean hasMoved;
-    
     public Piece(boolean isWhite, Position position) {
         this.isWhite = isWhite;
         this.position = position;
         this.hasMoved = false;
     }
-    
-    public boolean isWhite() { return isWhite; }
-    public Position getPosition() { return position; }
-    public boolean hasMoved() { return hasMoved; }
-    
+    public boolean isWhite() { 
+        return isWhite; 
+    }
+    public Position getPosition() { 
+        return position; 
+    }
+    public boolean hasMoved() { 
+        return hasMoved; 
+    }
     public void setPosition(Position position) {
         this.position = position;
         this.hasMoved = true;
     }
-    
     public abstract List<Position> getPossibleMoves(Board board);
     public abstract ReturnPiece.PieceType getPieceType();
     public abstract char getSymbol();
-    
+    //have to implement all these abstract classes in the subclasses defined
     public boolean canMoveTo(Position to, Board board) {
+        //check if given piece can move to a particular position
         List<Position> possibleMoves = getPossibleMoves(board);
         return possibleMoves.contains(to);
     }
