26138c114a4b01d93949db15fd1519d604f075ca
Prabhat-Shastri <prabhat.shastri.19@gmail.com>
2025-10-13 04:19:07 -0400

    Queen.java

--- chess/Queen.java
+++ chess/Queen.java
@@ -1,51 +1,53 @@
 package chess;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Queen extends Piece {
-    
     public Queen(boolean isWhite, Position position) {
         super(isWhite, position);
     }
-    
     @Override
     public List<Position> getPossibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
         int currentRank = position.getRank();
         int currentFile = position.getFile();
-        
         // Queen moves like both rook and bishop
         int[][] directions = {
             {0, 1}, {0, -1}, {1, 0}, {-1, 0},  // Rook moves
             {1, 1}, {1, -1}, {-1, 1}, {-1, -1} // Bishop moves
         };
-        
         for (int[] dir : directions) {
             for (int i = 1; i < 8; i++) {
-                Position newPos = new Position(currentFile + i * dir[0], currentRank + i * dir[1]);
-                if (!newPos.isValid()) break;
-                
+                int nextFile = currentFile + i * dir[0];
+                int nextRank = currentRank + i * dir[1];
+                Position newPos = new Position(nextFile,nextRank);
+                if (!newPos.isValid()) {
+                break;
+                }
                 Piece target = board.getPiece(newPos);
                 if (target == null) {
                     moves.add(newPos);
+                    //if empty, then valid
                 } else {
                     if (target.isWhite() != isWhite) {
                         moves.add(newPos);
+                        //if square got enemy piece, kill and add move
                     }
                     break;
                 }
             }
         }
-        
         return moves;
     }
-    
     @Override
     public ReturnPiece.PieceType getPieceType() {
-        return isWhite ? ReturnPiece.PieceType.WQ : ReturnPiece.PieceType.BQ;
+        if(isWhite)
+        {
+            return ReturnPiece.PieceType.WQ;
+        }
+        else{
+            return ReturnPiece.PieceType.BQ;
+        }
     }
-    
     @Override
     public char getSymbol() {
         return 'Q';
