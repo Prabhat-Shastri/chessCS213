5fe6d11c712608f2cd3752ca6dbc87c9866c6dc3
Prabhat-Shastri <prabhat.shastri.19@gmail.com>
2025-10-13 04:24:58 -0400

    Pawn.java

--- chess/Pawn.java
+++ chess/Pawn.java
@@ -1,26 +1,21 @@
 package chess;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Pawn extends Piece {
-    
     public Pawn(boolean isWhite, Position position) {
         super(isWhite, position);
     }
-    
     @Override
     public List<Position> getPossibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
         int direction = isWhite ? 1 : -1;
+        //if it is a white pawn, it will move up, hence 1 otherwise it will move down i.e. -1
         int currentRank = position.getRank();
         int currentFile = position.getFile();
-        
         // Forward move
         Position oneForward = new Position(currentFile, currentRank + direction);
         if (oneForward.isValid() && board.isEmpty(oneForward)) {
             moves.add(oneForward);
-            
             // Double forward move from starting position
             if (!hasMoved) {
                 Position twoForward = new Position(currentFile, currentRank + 2 * direction);
@@ -29,40 +24,43 @@ public class Pawn extends Piece {
                 }
             }
         }
-        
         // Diagonal captures
         Position leftCapture = new Position(currentFile - 1, currentRank + direction);
         if (leftCapture.isValid()) {
             Piece target = board.getPiece(leftCapture);
             if (target != null && target.isWhite() != isWhite) {
                 moves.add(leftCapture);
+                //if only there is an enemy piece in the square, it will add the move
             }
-            // En passant
+            // En passant - left
             if (leftCapture.equals(board.getEnPassantTarget())) {
                 moves.add(leftCapture);
             }
         }
-        
         Position rightCapture = new Position(currentFile + 1, currentRank + direction);
         if (rightCapture.isValid()) {
             Piece target = board.getPiece(rightCapture);
             if (target != null && target.isWhite() != isWhite) {
                 moves.add(rightCapture);
+                //similar to above, just on the right side
             }
-            // En passant
+            // En passant - right
             if (rightCapture.equals(board.getEnPassantTarget())) {
                 moves.add(rightCapture);
             }
         }
-        
         return moves;
     }
-    
     @Override
     public ReturnPiece.PieceType getPieceType() {
-        return isWhite ? ReturnPiece.PieceType.WP : ReturnPiece.PieceType.BP;
+        if (isWhite)
+        {
+            return ReturnPiece.PieceType.WP;
+        }
+        else{
+            return ReturnPiece.PieceType.BP;
+        }
     }
-    
     @Override
     public char getSymbol() {
         return 'P';
